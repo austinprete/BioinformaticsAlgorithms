feaec859088618904b6dc95c0ad88413fa83ccc8
austinprete <austinprete23@gmail.com>
2017-02-12 14:05:40 -0700

    Add faster_frequent_words to common.py

--- common.py
+++ common.py
@@ -6,6 +6,8 @@ from vol_1.chapter_1.ba1m import number_to_pattern
 # This relies on the above to dependencies being imported first
 from vol_1.chapter_1.ba1k import computing_frequencies
 
+from vol_1.chapter_1.ba1b import faster_frequent_words as frequent_words
+
 '''
-This file' purpose is to act as an easy export for commonly used functions
+This file's purpose is to act as an easy export for commonly used functions
 '''
